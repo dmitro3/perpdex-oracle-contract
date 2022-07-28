f38b8892f57856561e539178101bc005d7f2742f
a <a@example.com>
2022-07-28 15:37:42 +0900

    add deploy config for mumbai

--- deploy/01_deploy_price_feed.ts
+++ deploy/01_deploy_price_feed.ts
@@ -73,11 +73,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
             },
         ],
         mumbai: [
-            // ETH mumbai
+            // mumbai
             {
                 contract: "ChainlinkPriceFeed",
-                symbol: "ETHUSD",
-                args: ["0x0715A7794a1dc8e42615F059dD6e406A6594651A"],
+                symbol: "MATICUSD",
+                args: ["0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada"],
             },
         ],
         fuji: [
